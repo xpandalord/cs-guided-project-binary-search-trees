58521dc6c1ce4dc440845691d1fbbeb89e3c9e1f
Andy Tran <xpandalord@gmail.com>
2020-10-12 17:06:34 -0400

    updates

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,21 +1,21 @@
 """
 You are given a binary tree.
-
+​
 Write a function that can find the **maximum depth** of the binary tree. The
 maximum depth can be defined as the number of nodes found along the longest
 path from the root down to the furthest leaf node. Remember, a leaf node is a
 node that has no children.
-
+​
 Example:
-
+​
 Given the binary tree [5,12,32,None,None,8,4],
-
+​
     5
    / \
   12  32
      /  \
     8    4
-
+​
 your function should return the depth = 3.
 """
 class BinaryTreeNode:
@@ -23,7 +23,31 @@ class BinaryTreeNode:
         self.value = value
         self.left = left
         self.right = right
-
-def maxDepth(self, root):
-    # Your code here
-
+​
+    def maxDepth(self, root):
+        # Your code here
+        # base case? 
+        # what's the depth of an empty tree? 
+        if root is None:
+            return 0 
+        # how do we get to our base case? 
+        # move down the tree all the way until we get to 
+        # the node's at the bottom
+        # we need the height of left side 
+        left_height = self.maxDepth(root.left)
+        # we need the height of the right side 
+        right_height = self.maxDepth(root.right)
+​
+        larger_height = max(left_height, right_height)
+​
+        return 1 + larger_height
+​
+​
+tree = BinaryTreeNode(5)
+tree.left = BinaryTreeNode(12)
+right = BinaryTreeNode(32)
+right.right = BinaryTreeNode(4)
+right.left = BinaryTreeNode(8)
+tree.right = right
+​
+print(tree.maxDepth(tree))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -1,35 +1,48 @@
 """
-You are given a binary tree. You need to write a function that can determin if
+You are given a binary tree. You need to write a function that can determine if
 it is a valid binary search tree.
-
+​
 The rules for a valid binary search tree are:
-
+​
 - The node's left subtree only contains nodes with values less than the node's
 value.
 - The node's right subtree only contains nodes with values greater than the
 node's value.
 - Both the left and right subtrees must also be valid binary search trees.
-
+​
 Example 1:
-
+​
     5
    / \
   3   7
-
+​
 Input: [5,3,7]
 Output: True
-
+​
 Example 2:
-
+​
     10
    / \
   2   8
      / \
     6  12
-
+​
 Input: [10,2,8,None,None,6,12]
 Output: False
 Explanation: The root node's value is 10 but its right child's value is 8.
+​
+Example 2:
+​
+    10
+   / \
+  2   11
+     / \
+    6  12
+​
+Input: [10,2,11,None,None,6,12]
+Output: False
+Explanation: The root node's value is 10 but it has a child in the right sub-tree
+that is less than it.
 """
 # Definition for a binary tree node.
 class TreeNode:
@@ -37,7 +50,39 @@ class TreeNode:
         self.value = value
         self.left = left
         self.right = right
-
-def is_valid_BST(self, root):
-    # Your code here
-
+​
+    def check(self, upper, lower):
+        # base case?
+        # 1. return False when the value of self is not in the upper and lower bound 
+        if self.value < lower or self.value > upper:
+            return False
+        # 2. return True when we've traversed all the way down the tree with no issues
+        if not self.left and not self.right:
+            return True 
+        # how do we get closer to our base case? 
+        # traverse down the tree by checking the children nodes 
+        return self.left.check(self.value - 1, lower) and self.right.check(upper, self.value + 1)
+​
+    def is_valid_BST(self):
+        # Your code here
+        # Idea 1:    
+        # keep track of the min possible value and max possible value for a node 
+        # when we traverse left, update the max possible value 
+        # when we traverse right, update the min possible value 
+        # check if the current node's value falls within the given range 
+        return self.check(float("inf"), float("-inf"))
+​
+        # Idea 2:
+        # get all of the elements out of the binary tree in order 
+        # (a valid binary search tree would give back all its elements in sorted order)
+        # check if order of the elements from the tree matches the order of the same 
+        # elements in sorted order 
+​
+tree = TreeNode(10)
+tree.left = TreeNode(2)
+right = TreeNode(11)
+right.left = TreeNode(6)
+right.right = TreeNode(12)
+tree.right = right
+​
+print(tree.is_valid_BST())
